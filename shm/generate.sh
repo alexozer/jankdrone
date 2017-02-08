ef8d31aa5a7f3e358947235c9bdbcde8816423b7
Alex Ozer <thealexozer@gmail.com>
2017-02-08 13:39:10 -0500

    Make shm use own version of nanopb

--- shm/generate.sh
+++ shm/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-protoc --plugin=protoc-gen-nanopb=../drone/lib/nanopb/generator/protoc-gen-nanopb \
+protoc --plugin=protoc-gen-nanopb=lib/nanopb/generator/protoc-gen-nanopb \
 	--nanopb_out=../drone/src --nanopb_out=../handheld/src shm.proto
 protoc -I=. --go_out=. shm.proto
 ./generate_shm.py
